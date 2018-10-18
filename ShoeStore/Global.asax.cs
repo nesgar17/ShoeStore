ab6f1be6548c4e0679ecb684e60508b3b2924f64
nesgar17 <39680968+nesgar17@users.noreply.github.com>
2018-10-18 02:58:42 -0500

    add class mailhelper

--- ShoeStore/Global.asax.cs
+++ ShoeStore/Global.asax.cs
@@ -1,5 +1,6 @@
 ﻿namespace ShoeStore
 {
+    using ShoeStore.Classes;
     using System.Data.Entity;
     using System.Web.Http;
     using System.Web.Mvc;
@@ -11,11 +12,22 @@
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+            checkRolesAndSuperUser();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             Database.SetInitializer(new MigrateDatabaseToLatestVersion<Models.DataContext, Migrations.Configuration>());
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        private void checkRolesAndSuperUser()
+        {
+            UsersHelper.CheckRole("Admin");
+            UsersHelper.CheckRole("Cliente");
+            UsersHelper.CheckRole("Directivo");
+            UsersHelper.CheckRole("Gerente");
+            UsersHelper.CheckRole("Zapateria");
+            UsersHelper.CheckSuperUser();
         }
     }
 }
